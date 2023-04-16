2d2c114b2d3826bc5e10923fd636f7c47628edc6
example <sumeyyesabirli@gmail.com>
2023-04-17 02:12:06 +0300

    Kategori  oluşturuldu

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
@@ -6,8 +7,36 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule,CommonModule],
 })
 export class HomePage {
+
+  todoList = [{
+    itemName: 'coding',
+    itemDuedate: '10-12-20',
+    itemPriority: 'low',
+    itemCategory: 'word'
+  },
+  {
+    itemName: 'sleep',
+    itemDuedate: '10-12-20',
+    itemPriority: 'high',
+    itemCategory: 'work'
+  },
+  {
+    itemName: 'weed',
+    itemDuedate: '10-12-20',
+    itemPriority: 'middle',
+    itemCategory: 'work'
+  },
+  {
+    itemName: 'music',
+    itemDuedate: '30-12',
+    itemPriority: 'high',
+    itemCategory: 'personal'
+  },
+]
+  today:number = Date.now()
+
   constructor() {}
 }
